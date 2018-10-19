8e3527bb482e880a7d887a77bbd5932acbd9a52f
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2018-10-19 22:17:45 +0900

    export を修正

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import directive from './directive'
 import VFragment from './component'
 import ssr       from './ssr'
 
-export default {
-  ssr,
-  directive,
-  VFragment,
-  Plugin: {
-    install: function(Vue) {
-      Vue.directive('fragment', directive)
-      Vue.component('v-fragment', component)
-    }
-  }
-}
+export { ssr }
+export { directive }
+export { VFragment }
+export const Plugin = { install: function(Vue) {
+    Vue.directive('fragment', directive)
+    Vue.component('v-fragment', component)
+  }}
